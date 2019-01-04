046bbbf7ad38083c1d12fe8b643e5598c8049cef
JettHuang <123258314@qq.com>
2019-01-04 14:38:29 +0800

    echo server

---
+++ echo/echo_server.js
@@ -0,0 +1,9 @@
+const net = require('net');
+const server = net.createServer(socket => {
+    socket.on('data', data => {
+        socket.write(data);
+        console.log(data);
+    });
+});
+
+server.listen(8888);
