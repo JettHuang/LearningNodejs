574be4726491ffff125070cb6c26fc3b5666b718
JettHuang <123258314@qq.com>
2019-01-04 19:48:12 +0800

    RESTful路由

--- WebDemo/index.js
+++ WebDemo/index.js
@@ -1,10 +1,36 @@
 const express = require('express');
 const app = express();
+const articles = [{ title: 'Example' }];
 
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
+});
+
+// 获取所有文章
+app.get('/articles', (req, res, next) => {
+    res.send(articles);
+});
+
+// 创建一篇文章
+app.post('/articles', (req, res, next) => {
+    res.send('OK');
+});
+
+// 获取指定文章
+app.get('/articles/:id', (req, res, next) => {
+    const id = req.params.id;
+    console.log('Fetching:', id);
+    res.send(articles[id]);
+});
+
+// 删除指定文章
+app.get('/articles/:id', (req, res, next) => {
+    const id = req.params.id;
+    console.log('Deleting:', id);
+    delete articles[id];
+    res.send({ message: 'Deleted' });
 });
 
 app.listen(port, () => {
