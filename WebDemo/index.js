d9b36d50782f335e98f760e8485d25762b4f1b01
JettHuang <123258314@qq.com>
2019-01-06 17:21:47 +0800

    Add body-parser. 解析post协议

--- WebDemo/index.js
+++ WebDemo/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 const articles = [{ title: 'Example' }];
 
 const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json()); // 支持编码为json的请求消息体
+app.use(bodyParser.urlencoded({ extended: true })); // 支持编码为表单的请求消息体
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
@@ -15,7 +20,11 @@ app.get('/articles', (req, res, next) => {
 
 // 创建一篇文章
 app.post('/articles', (req, res, next) => {
-    res.send('OK');
+    const article = { title: req.body.title };
+    articles.push(article);
+    res.send(article);
+
+    console.log("post request: ", req.body);
 });
 
 // 获取指定文章
@@ -26,7 +35,7 @@ app.get('/articles/:id', (req, res, next) => {
 });
 
 // 删除指定文章
-app.get('/articles/:id', (req, res, next) => {
+app.delete('/articles/:id', (req, res, next) => {
     const id = req.params.id;
     console.log('Deleting:', id);
     delete articles[id];
